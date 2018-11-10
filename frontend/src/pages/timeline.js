2755b762c349ba81e965ec86f1ac123999285db5
DittrichLucas <ld.dittrich@gmail.com>
2018-11-10 15:25:26 -0200

    Finalized frontend code

--- frontend/src/pages/timeline.js
+++ frontend/src/pages/timeline.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
+import api from '../services/api'
 import twitterLogo from '../twitter.svg'
 import './Timeline.css';
+import Tweet from '../components/tweet'
+import socket from 'socket.io-client'
 
 export default class Timeline extends Component {
     state = {
+        tweets: [],
         newTweet: ''
     }
 
-    handleNewTweet = (e) => {
+    async componentDidMount() {
+        this.subscribeToEvents()
+
+        const response = await  api.get('tweets')
+
+        this.setState({ tweets: response.data })
+    }
+
+    subscribeToEvents = () => {
+        const io = socket('http://localhost:3000')
+
+        io.on('tweet', data => {
+            this.setState({ tweets: [ data, ...this.state.tweets ] })
+        })
+        io.on('like', data => {
+            this.setState({ tweets: this.state.tweets.map(
+                tweet => (tweet._id === data._id ? data : tweet )
+            ) })
+        })
+    }
+
+    handleNewTweet = async (e) => {
         if(e.keyCode !== 13) return
 
         const content = this.state.newTweet
         const author = localStorage.getItem('@GoTwitter:username')
 
-        console.log(content, author)
+        await api.post('tweet', { content, author })
+
+        this.setState({ newTweet: '' })
     }
 
     handleInputChange = (e) => {
@@ -32,9 +59,10 @@ export default class Timeline extends Component {
                         placeholder='O que está acontecendo?'
                     />
                 </form>
-
-                <ul>
-
+                <ul className='tweet-list'>
+                    { this.state.tweets.map( tweet => 
+                        <Tweet key={ tweet._id } tweet={ tweet } />
+                    )}
                 </ul>
             </div>
         )
